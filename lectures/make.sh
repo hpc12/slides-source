c7f95fb7bd0fae83246d2d78346bb0fb68ecb786
Andreas Kloeckner <inform@tiker.net>
2012-09-03 17:00:26 -0400

    Initial setup.

---
+++ lectures/make.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+set -e
+
+SOURCE=$1
+
+mkdir -p out
+
+if test "$SOURCE" = ""; then
+  echo "usage: $0 source-file-without-extension-and-path"
+  exit 1
+fi
+
+mv "out/$SOURCE.pdf" "out/$SOURCE-prev.pdf" || true
+
+export TEXINPUTS=$TEXINPUTS:.
+export TEXINPUTS=$TEXINPUTS:$SOURCE-media
+export TEXINPUTS=$TEXINPUTS:media:slides:code:loopy-examples
+export TEXINPUTS=$TEXINPUTS:$HOME/research/presentations/slides
+export TEXINPUTS=$TEXINPUTS:$HOME/research/presentations/slides/snippets
+export TEXINPUTS=$TEXINPUTS:$HOME/research/presentations/slides/style
+export TEXINPUTS=$TEXINPUTS:$HOME/research/presentations/slides/media
+
+#(cd out; BIBINPUTS=.. bibtex $SOURCE)
+
+pdflatex -output-directory out $SOURCE || ERROR=1
+if test "$ERROR" == 1; then
+  echo "-----------------------------------------------------------------------"
+  echo "An error occurred. Hit enter to see the log."
+  echo "-----------------------------------------------------------------------"
+  read LINE
+  less out/$SOURCE.log
+fi
